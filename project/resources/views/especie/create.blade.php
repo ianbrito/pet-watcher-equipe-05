ac387b29fedce32aa909dcf6f336e5ee9cf13d8b
NepZR <lucas.darlindo@gmail.com>
2021-08-18 11:33:22 -0300

    Front-end: Espécies implementado (todos os casos)

--- project/resources/views/especie/create.blade.php
+++ project/resources/views/especie/create.blade.php
@@ -3,14 +3,41 @@
 @section('title', 'Cadastrar Espécie')
 
 @section('content')
-
-<h3>Cadastrar nova espécie</h1>
-
-<form action="{{ action('EspecieController@store') }}" method="post">
-    @csrf
-    <label for="especie">Nome da espécie: </label>
-    <input type="text" name="especie" id="especie">
-    <input type="submit" value="Cadastrar">
-</form>
+    <div class="limiter-default">
+        <div class="container-insert100">
+            <div class="wrap-new-entry100">
+                @if($errors->all())
+                    @foreach($errors->all() as $error)
+                        <div>
+                            {{$error}}
+                        </div>
+                    @endforeach
+                @endif
+                <form action="{{ action('EspecieController@store') }}" method="post"
+                      class="nwe100-form validate-form">
+                    @csrf
+                    <span class="home100-form-title">
+                    Cadastrar nova espécie
+                    </span>
+                    <div class="wrap-input100 validate-input">
+                        <input type="text" class="form-control input100" id="especie" name="especie"
+                               placeholder="Nome da espécie">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fas fa-paw" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="container-login100-form-btn">
+                        <input type="submit" class="login100-form-btn" value="Cadastrar">
+                    </div>
+                    <div class="text-center p-t-12">
+                    </div>
+                    <div class="text-center p-t-136">
+                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 @endsection
